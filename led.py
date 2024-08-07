d73d9198bfe767624f9c9193fc197f7c24fb7024
badingaLuc <luc.badinga@piamot.com>
2024-08-07 15:13:28 +0200

    luc commit

--- led.py
+++ led.py
@@ -7,9 +7,9 @@ pinout = 18
 color = "Green"
 
 GPIO.setup(pinout,GPIO.OUT)
-print "LED on N" + str(pinout) + " " + color
+print ("LED on N" + str(pinout) + " " + color)
 GPIO.output(pinout,GPIO.HIGH)
 time.sleep(1)
-print "LED off N" + str(pinout) + " " + color
+print ("LED off N" + str(pinout) + " " + color)
 GPIO.output(pinout,GPIO.LOW)
 time.sleep(1)
