e06d085afb668f778323358cc8828fe4261862e8
badingaLuc <luc.badinga@piamot.com>
2024-08-07 14:37:07 +0200

    first commit

---
+++ led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pinout = 18
+color = "Green"
+
+GPIO.setup(pinout,GPIO.OUT)
+print "LED on N" + str(pinout) + " " + color
+GPIO.output(pinout,GPIO.HIGH)
+time.sleep(1)
+print "LED off N" + str(pinout) + " " + color
+GPIO.output(pinout,GPIO.LOW)
+time.sleep(1)
